d5bf7c9ef19214d7b2840b77792e9c89833c500d
abdul.zaidi <abdul.zaidi@fenix.life>
2022-09-04 02:02:56 +0500

    Space News

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [newsList,setNewsList] = useState([]);
+  useEffect(()=> {
+    fetch("https://api.spaceflightnewsapi.net/v3/articles")
+    .then((response) => response.json())
+    .then((data) => setNewsList(data))
+  },[])// putting [] so that it doesn't render after the first time
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='title'>
+      <h1>Space News</h1>
+      </div>
+      <div className='newsContainer'>
+        {newsList.map((val,key) => {
+          return <div key={key} className="article" onClick={()=> {window.location.href = val["url"]}}>
+            <h1>{val["title"]}</h1>
+            <img src={val["imageUrl"]} alt="Image" />
+            <p>{val["summary"]}</p>
+            <h3>{val["publishedAt"]}</h3>
+
+          </div>
+        })}
+      </div>
     </div>
   );
 }
